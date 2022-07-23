2c9f64c887b05b79ac15265ffd28db0c887e712f
AlexServGE <alexander.sergeevru@yandex.ru>
2022-07-23 17:14:38 +0300

    FinalTestWork_Project.cs: Created method UserArrayElementsWithLt3Symbols. Added 1 of 2 while cycles. Not tested

---
+++ FinalTestWork_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string[] UserArrayElementsWithLt3Symbols(string[] userArray)
+{
+    string[] subresultArray = new string[userArray.Length];
+    int i = 0, j = 0;
+    while (i < userArray.Length)
+    {
+        if (userArray[i].Length > 3)
+        {
+            i = i + 1;
+            continue;
+        }
+        else
+        {
+            subresultArray[j] = userArray[i];
+            j = j + 1;
+        }
+    }
+    string [] resultArray = new string[j];
+    return resultArray;
+}
